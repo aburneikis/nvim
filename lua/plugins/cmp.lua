d77205d56c67ef8a7c009afd2acae833c5ac3cd7
Alex Burneikis <z5481067@ad.unsw.edu.au>
2026-01-20 21:06:27 +1100

    copilot and dash fixes

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -31,7 +31,7 @@ return {
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
+        ["<C-n>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
           elseif luasnip.expand_or_jumpable() then
@@ -40,7 +40,7 @@ return {
             fallback()
           end
         end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
+        ["<C-p>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
           elseif luasnip.jumpable(-1) then
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -12,7 +12,7 @@ return {
         auto_trigger = true,
         debounce = 75,
         keymap = {
-          accept = "<M-l>",
+          accept = false,
           accept_word = "<M-w>",
           accept_line = "<M-e>",
           next = "<M-]>",
@@ -29,5 +29,13 @@ return {
         ["."] = false,
       },
     })
+
+    vim.keymap.set("i", "<Tab>", function()
+      if require("copilot.suggestion").is_visible() then
+        require("copilot.suggestion").accept()
+      else
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+      end
+    end, { desc = "Accept Copilot suggestion or insert tab" })
   end,
 }
