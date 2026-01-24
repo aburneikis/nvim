5b37fb65d41ac6adb1e00da4dd6a8cb6dcd92d12
Alex Burneikis <z5481067@ad.unsw.edu.au>
2026-01-24 15:05:22 +1100

    blue nodejumper highlight

--- lua/plugins/nodejumper.lua
+++ lua/plugins/nodejumper.lua
@@ -2,8 +2,12 @@ return {
     "burneikis/nodejumper.nvim",
     event = "VeryLazy",
     opts = {
-      dim_background = false,
+      -- dim_background = false,
     },
+    config = function(_, opts)
+      require("nodejumper").setup(opts)
+      vim.api.nvim_set_hl(0, "NodejumperLabel", { fg = "#7aa2f7", bold = true }) -- tokyonight blue highlight
+    end,
     keys = {
       { "S", mode = { "n", "x", "o" }, function() require("nodejumper").jump() end, desc = "Jump to treesitter node" },
     },
