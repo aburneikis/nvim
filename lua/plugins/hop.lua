3ae79c7bf076240cfbd97cbab4d66add3b4dd6dc
Alex Burneikis <z5481067@ad.unsw.edu.au>
2026-01-24 15:01:03 +1100

    add hop word

---
+++ lua/plugins/hop.lua
@@ -0,0 +1,11 @@
+return {
+	"smoka7/hop.nvim",
+	version = "*",
+	event = "VeryLazy",
+	opts = {
+		keys = "etovxqpdygfblzhckisuran",
+	},
+	keys = {
+		{ "gw", "<cmd>HopWord<cr>", desc = "Hop Word" },
+	},
+}
